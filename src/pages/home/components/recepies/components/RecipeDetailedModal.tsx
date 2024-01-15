deb7b7eef410421f45818a1a5c5dd9cfb3f73540
nazarzadeR <resadnazar.zade@gmail.com>
2024-01-16 00:23:48 +0400

    fixed importing issue

--- src/pages/home/components/recepies/components/RecipeDetailedModal.tsx
+++ src/pages/home/components/recepies/components/RecipeDetailedModal.tsx
@@ -1,5 +1,5 @@
 import { isEmpty } from "lodash";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { Box, Center } from "@chakra-ui/react";
 
 import SelectedRecipeModalInner from "./RecipeDetailModalInner";
